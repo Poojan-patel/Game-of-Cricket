7ecd159f814640e7d3e36fa6d63b009801b6b889
Poojan Patel <poojan4004@gmail.com>
2022-02-10 19:41:52 +0530

    Modified Suggested Changes
    Major: Scanner object is a class property now

--- src/com/tekion/intern/MatchController.java
+++ src/com/tekion/intern/MatchController.java
@@ -8,21 +8,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MatchController {
-    private static List<String> getTeamPlayers(int numOfPlayers){
-        Scanner sc = new Scanner(System.in);
-        List<String> players = new ArrayList<>();
-        for(int i = 1; i <= numOfPlayers; i++){
-            System.out.print("Player-"+i+" Name:");
-            players.add(sc.nextLine());
-            System.out.print("Player-"+i+" Type:");
-            players.add(sc.nextLine());
-        }
-        return players;
-    }
-
+    private static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
         System.out.print("Number Of Overs:");
         int numOfOvers = sc.nextInt();
         sc.nextLine();
@@ -33,20 +20,34 @@ public class MatchController {
 
         System.out.print("Enter Team-1 Name:");
         String team1 = sc.nextLine();
-        List<String> team1Players = getTeamPlayers(numOfPlayers);
+        List<String> team1PlayersTypes = new ArrayList<>();
+        List<String> team1PlayersNames = new ArrayList<>();
+        getTeamPlayers(numOfPlayers, team1PlayersNames, team1PlayersTypes);
 
         System.out.print("Enter Team-2 Name:");
         String team2 = sc.nextLine();
-        List<String> team2Players = getTeamPlayers(numOfPlayers);
+        List<String> team2PlayersTypes = new ArrayList<>();
+        List<String> team2PlayersNames = new ArrayList<>();
+        getTeamPlayers(numOfPlayers, team2PlayersNames, team2PlayersTypes);
 
-        Match m = new Match(numOfOvers, team1, team1Players, team2, team2Players);
+        Match newMatch = new Match(numOfOvers, team1, team1PlayersNames, team1PlayersTypes, team2, team2PlayersNames, team2PlayersTypes);
 
         int headOrTail = MatchUtil.stimulateToss();
         System.out.print("0.. Fielding, 1.. Batting:");
         int choiceOfInning = sc.nextInt();
         sc.nextLine();
 
-        m.stimulateGame(headOrTail, choiceOfInning);
-        m.showFinalScoreBoard();
+        newMatch.stimulateGame(headOrTail, choiceOfInning);
+        newMatch.showFinalScoreBoard();
+    }
+
+    private static void getTeamPlayers(int numOfPlayers, List<String> playerNames, List<String> playerTypes){
+        for(int i = 1; i <= numOfPlayers; i++){
+            System.out.print("Player-"+i+" Name:");
+            playerNames.add(sc.nextLine());
+            System.out.print("Player-"+i+" Type:");
+            playerTypes.add(sc.nextLine());
+        }
+
     }
 }
--- src/com/tekion/intern/game/Strike.java
+++ src/com/tekion/intern/game/Strike.java
@@ -1,20 +1,12 @@
 package com.tekion.intern.game;
 
 class Strike{
-    private int currentWickets;
     private int[] strikeHolders;
     private int currentStrike;
 
     public Strike(){
         this.currentStrike = 0;
         this.strikeHolders = new int[]{0, 1};
-        currentWickets = 0;
-    }
-
-    public void reInit() {
-        this.currentStrike = 0;
-        this.strikeHolders = new int[]{0, 1};
-        currentWickets = 0;
     }
 
     public void overChanged(){
@@ -30,15 +22,11 @@ class Strike{
         return strikeHolders[currentStrike];
     }
 
-    public int updateOnWicket(){
-        currentWickets++;
+    public int updateStrikeOnWicket(){
         int maxOrder = Integer.max(strikeHolders[0], strikeHolders[1]);
         int outPlayer = strikeHolders[currentStrike];
         strikeHolders[currentStrike] = maxOrder+1;
         return outPlayer;
     }
 
-    public int totalWickets(){
-        return currentWickets;
-    }
 }
